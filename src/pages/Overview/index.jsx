ea6f54418a0804142796be48ce8d416997c808a3
kamino <lishichenjs@163.com>
2019-05-19 20:48:49 +0800

    更新鸽子页面

--- src/pages/Overview/index.jsx
+++ src/pages/Overview/index.jsx
@@ -6,65 +6,17 @@ import './style.scss';
 import axios from 'axios';
 import DataSet from '@antv/data-set';
 import config from '../../config';
+import gugugu from '../../assets/images/gugugu.jpg';
 
 class Overview extends Component {
 
 
     render() {
-        const dv = new DataSet().createView();
-        const state = {loading: true};
-
-        const freshData = () => function () {
-            console.log('click');
-            axios
-                .get(config.api + '/blive/overview')
-                .then(function (response) {
-                    console.log(response);
-                    dv.source(response.data['SignDayCount']);
-                })
-                .finally(function () {
-                    state.loading = false;
-                })
-            ;
-        };
-
-        const cols = {
-            key: {
-                name: "日期",
-                range: [0, 1]
-            },
-            value: {
-                name: "签到人数",
-                min: 0
-            }
-        };
-
         return (
             <MainLayout>
-                <Spin spinning={state.loading}>
-                    <div>
-                        <Chart height={400} data={dv} scale={cols} width={600}>
-                            <Axis name="日期"/>
-                            <Axis name="签到人数"/>
-                            <Tooltip
-                                crosshairs={{
-                                    type: "y",
-                                }}
-                            />
-                            <Geom type="line" position="key*value" size={2}/>
-                            <Geom
-                                type="point"
-                                position="key*value"
-                                size={4}
-                                shape={"circle"}
-                                style={{
-                                    stroke: "#fff",
-                                    lineWidth: 1
-                                }}
-                            />
-                        </Chart>
-                    </div>
-                </Spin>
+                <img width="300px" src={gugugu} alt="鸽了"/>
+                <p>虽然这个页面鸽了，其他几个页面是可以看的！</p>
+
             </MainLayout>
         );
     }
