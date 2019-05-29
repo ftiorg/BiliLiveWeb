b0edbe71ed51c5ac80a4611f110742ab7c2eb9a8
aikamino <kamino@imea.me>
2019-05-29 17:25:33 +0800

    Wed May 29 17:25:33 CST 2019

---
+++ precache-manifest.3314d09329fe38f24a41a4d81d294279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1a406a0916446b9ad1687da8c2503ea",
+    "url": "/static/media/gugugu.b1a406a0.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7736b5253b97eedabde4",
+    "url": "/static/js/main.55b513a9.chunk.js"
+  },
+  {
+    "revision": "2dbcbc32e48be5eae60b",
+    "url": "/static/js/2.42b229d4.chunk.js"
+  },
+  {
+    "revision": "7736b5253b97eedabde4",
+    "url": "/static/css/main.2fa91af6.chunk.css"
+  },
+  {
+    "revision": "2dbcbc32e48be5eae60b",
+    "url": "/static/css/2.3b6f747f.chunk.css"
+  },
+  {
+    "revision": "30f3631b149064930f57f60ec3bfb2c6",
+    "url": "/index.html"
+  }
+];
